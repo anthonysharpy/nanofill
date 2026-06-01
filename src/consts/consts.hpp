e252a7cc4fd826a30dc81fa3bd9d4f4787a9ef68
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-06-01 21:36:26 +0900

    use size_t instead of unsigned int

--- src/consts/consts.hpp
+++ src/consts/consts.hpp
@@ -6,6 +6,6 @@
 namespace nanofill::consts {
 
 using TradingDataCSVFormat = std::tuple<double, std::uint8_t, std::uint32_t, std::uint32_t, std::uint32_t, std::int8_t>;
-constexpr unsigned int SPSC_BUFFER_SIZE = 256;
+constexpr std::size_t SPSC_BUFFER_SIZE = 256;
 
 }
